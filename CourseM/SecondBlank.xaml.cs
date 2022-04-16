04dc30bf5fb29b4cd05d7b10b36ac8464c9f2c2c
Николай Примаченко <o687801077@gmail.com>
2022-04-16 15:35:06 +0300

    Add new page

---
+++ CourseM/SecondBlank.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CourseM
+{
+    /// <summary>
+    /// Interaction logic for SecondBlank.xaml
+    /// </summary>
+    public partial class SecondBlank : Page
+    {
+        FirstBlank firstBlank;
+        public SecondBlank(FirstBlank firstBlank)
+        {
+            InitializeComponent();
+
+            this.firstBlank = firstBlank;
+        }
+
+        private void OpenLastPage(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(firstBlank);
+
+        }
+    }
+}
